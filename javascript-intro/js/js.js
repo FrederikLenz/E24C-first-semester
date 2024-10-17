278801b1810b003743c85e168d30415487e5fd2a
FrederikLenz <lpfrede@gmail.com>
2024-10-17 23:51:19 +0200

    Upload af web univers

--- javascript-intro/js/js.js
+++ javascript-intro/js/js.js
@@ -2,4 +2,53 @@
  * file: js/js.js
  * purpose: Behaviors
  **/
-console.log('Success: JavaScript running!')
+console.log('Success: JavaScript running!');
+console.log("Hello World");
+
+/* Her er en konsol, der skriver Mojn */
+
+console.log("Mojn Do"); // Her er en kommentar
+
+
+/* Variabler; strenge */
+let a = "Huske at skrive til Michael i aften ...";
+let b = "23";
+
+
+/** Variabler: tal - JS kan regne */
+let c = 23;
+console.log(b + b);
+console.log(c * c);
+
+/** Booleans: true / false */
+let julemandenEksisterer = true;
+console.log(julemandenEksisterer);
+
+let aftensmad = false;
+
+let vaer_23_booked = false;
+
+/** kontrolstrukturer */
+/*
+if (new Date().getHours() < 18) {
+    document.getElementById("demo").innerHTML = "good day";
+}
+
+
+
+/** konkatenering: "klistre sammen" med + */
+let header = "<h1>" + a + "</h1>";
+console.log(header);
+
+document.getElementById("hej").innerHTML = header;
+document.getElementById("hej").style.color = "blue";
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
